4df8826d5a36dbbe23e9d821c416185b0257b451
dohoan259 <dohoan259@gmail.com>
2024-06-27 06:30:28 +0700

    format code

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ const route = require('./routes/index.route.js');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-            app.use(
-  express.urlencoded({
-    extended: true,
-  }),
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 
@@ -22,12 +22,10 @@ app.use(morgan('combined'));
 
 // template engine
 app.engine(
-  'hbs',
-  handlebars.engine({
-
-    
-    extname: '.hbs',
-  }),
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
 ); // define name
 app.set('view engine', 'hbs'); // use for system
 app.set('views', path.join(__dirname, 'resources', 'views'));
@@ -36,5 +34,5 @@ app.set('views', path.join(__dirname, 'resources', 'views'));
 route(app);
 
 app.listen(port, () =>
-  console.log(`Example listening on at http://localhost:${port}`),
+    console.log(`Example listening on at http://localhost:${port}`),
 );
